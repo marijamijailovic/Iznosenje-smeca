57621f88c88e3a011883ff36c4d82998690abc8e
marijamijailovic <mijailovicmarija@hotmail.com>
2018-01-10 22:51:14 +0100

    sitne izmene

--- Prototip/request.php
+++ Prototip/request.php
@@ -2,6 +2,12 @@
    include("biblioteka.php");
    konektujSe();
 
+   $upit = "select Zahtev_idZahtev from Poslovi";
+ 	 $rezultat=mysqli_query($veza,$upit) or die("greska1");
+   while($red = mysqli_fetch_assoc($rezultat)){
+     $poslovi[] = $red['Zahtev_idZahtev'];
+   }
+
    $upit="select * from Zahtev";
    $rezultat=mysqli_query($veza, $upit) or die("Problem prilikom izvrsavanja upita");
 
@@ -12,11 +18,13 @@
   		$zahtev=new stdClass();
 
   		$zahtev->idZahtev=$red['idZahtev'];
-  		$zahtev->tekstZahteva=$red['tekstZahteva'];
-  		$zahtev->ocekivanDatum=$red['ocekivanDatumIznosenja'];
-      $zahtev->lokacijaOtpada=$red['lokacijaOtpada'];
+      if (!in_array($zahtev->idZahtev, $poslovi)) {
+    		$zahtev->tekstZahteva=$red['tekstZahteva'];
+    		$zahtev->ocekivanDatum=$red['ocekivanDatumIznosenja'];
+        $zahtev->lokacijaOtpada=$red['lokacijaOtpada'];
 
-  		array_push($zahtevi, $zahtev);
+    		array_push($zahtevi, $zahtev);
+      }
    }
 
 	 $za_slanje=json_encode($zahtevi);
--- Prototip/vacationRequest.php
+++ Prototip/vacationRequest.php
@@ -7,10 +7,10 @@
 	$ime = $_GET['ime'];
 	$prezime = $_GET['prezime'];
 	$opis = $_GET['opis'];
-	
+
 	$pocetakOdmora = $_GET['pocetakOdmora'];
 	$krajOdmora = $_GET['krajOdmora'];
-	
+
 	$daniPocetak = substr($pocetakOdmora, 8, 2);
 	$daniKraj = substr($krajOdmora, 8, 2);
 	$brojDana = $daniKraj - $daniPocetak;
@@ -20,19 +20,19 @@
 	$red = mysqli_fetch_assoc($rezultat);
 	$brojPreostalihDana = $red['brojSlobodnihDana'];
 	$idZaposleni = $red['idZaposleni'];
-	
+
 	if ($brojPreostalihDana < $brojDana)
 		die ("trazite vise slobodnih dana nego sto vam je preostalo");
-	
+
 	$upit = "select Zaposleni_idZaposleni as id from Upravnik";
 	$rezultat=mysqli_query($veza,$upit) or die("greska1");
 	$red = mysqli_fetch_assoc($rezultat);
 	$idUpravnika = $red['id'];
-	
-	
-	$upit = "insert into zahtevzaodmorom values($idZaposleni, $idUpravnika, DATE '$pocetakOdmora', DATE '$krajOdmora', '$opis')";
+
+
+	$upit = "insert into ZahtevZaOdmorom values($idZaposleni, $idUpravnika, DATE '$pocetakOdmora', DATE '$krajOdmora', '$opis')";
 	$rezultat=mysqli_query($veza,$upit) or die("greska insert");
-	
+
 	echo "Uspesno ste predali zahtev";
 
 	diskonektujSe();
